36359a56d018c65c253b1737190282340c117ad6
RedstoneParadox <spinjitzu00@gmail.com>
2021-05-27 16:31:02 -0700

    Both buildings now have a minimum operating temperature

--- Refrigeration Units/LiquidRefrigerationUnitConfig.cs
+++ Refrigeration Units/LiquidRefrigerationUnitConfig.cs
@@ -62,6 +62,7 @@ namespace RefrigerationUnits
             go.AddOrGet<LogicOperationalController>();
             go.AddOrGetDef<PoweredActiveController.Def>();
             go.GetComponent<KPrefabID>().AddTag(GameTags.OverlayBehindConduits);
+            go.AddOrGet<MinimumOperatingTemperature>().minimumTemperature = 28f;
         }
     }
 }
