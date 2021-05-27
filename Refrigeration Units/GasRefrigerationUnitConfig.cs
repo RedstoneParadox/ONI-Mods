02bb38f0b5ff255324fe83346f919c53e38926b5
RedstoneParadox <spinjitzu00@gmail.com>
2021-05-27 14:00:23 -0700

    Actually commit everything

---
+++ Refrigeration Units/GasRefrigerationUnitConfig.cs
@@ -0,0 +1,49 @@
+﻿using TUNING;
+using UnityEngine;
+
+namespace RefrigerationUnits
+{
+    class GasRefrigerationUnitConfig: IBuildingConfig
+    {
+        public const string ID = "GasRefrigerationUnit";
+
+        public override BuildingDef CreateBuildingDef()
+        {
+            float[] tieR3 = TUNING.BUILDINGS.CONSTRUCTION_MASS_KG.TIER3;
+            string[] allMetals = MATERIALS.ALL_METALS;
+            EffectorValues tieR2 = NOISE_POLLUTION.NOISY.TIER2;
+            EffectorValues none = TUNING.BUILDINGS.DECOR.NONE;
+            EffectorValues noise = tieR2;
+            BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef("GasRefigerationUnit", 2, 2, "airconditioner_kanim", 100, 120f, tieR3, allMetals, 1600f, BuildLocationRule.OnFloor, none, noise);
+            BuildingTemplates.CreateElectricalBuildingDef(buildingDef);
+            buildingDef.EnergyConsumptionWhenActive = 240f;
+            buildingDef.SelfHeatKilowattsWhenActive = 0.0f;
+            buildingDef.ThermalConductivity = 5f;
+            buildingDef.InputConduitType = ConduitType.Gas;
+            buildingDef.OutputConduitType = ConduitType.Gas;
+            buildingDef.PowerInputOffset = new CellOffset(1, 0);
+            buildingDef.PermittedRotations = PermittedRotations.FlipH;
+            buildingDef.ViewMode = OverlayModes.GasConduits.ID;
+            buildingDef.LogicInputPorts = LogicOperationalController.CreateSingleInputPortList(new CellOffset(0, 1));
+            return buildingDef;
+        }
+
+        public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
+        {
+            go.AddOrGet<LoopingSounds>();
+            AirConditioner refrigerationUnit = go.AddOrGet<AirConditioner>();
+            refrigerationUnit.temperatureDelta = 14f;
+            refrigerationUnit.maxEnvironmentDelta = 50f;
+            BuildingTemplates.CreateDefaultStorage(go).showInUI = true;
+            ConduitConsumer conduitConsumer = go.AddOrGet<ConduitConsumer>();
+            conduitConsumer.conduitType = ConduitType.Gas;
+            conduitConsumer.consumptionRate = 1f;
+        }
+
+        public override void DoPostConfigureComplete(GameObject go)
+        {
+            go.AddOrGet<LogicOperationalController>();
+            go.AddOrGetDef<PoweredActiveController.Def>();
+        }
+    }
+}
---
+++ Refrigeration Units/Patches.cs
@@ -0,0 +1,79 @@
+﻿using Harmony;
+using System;
+using UnityEngine;
+
+namespace RefrigerationUnits
+{
+    public static class Mod_OnLoad
+    {
+        public static void OnLoad()
+        {
+            Debug.Log("Hello world!");
+        }
+    }
+
+    public class Patches
+    {
+
+        //ColorPatch. Thx Pholith!
+        [HarmonyPatch(typeof(BuildingComplete), "OnSpawn")]
+        public class ColorPatch
+        {
+            public static void Postfix(BuildingComplete __instance)
+            {
+                var kAnimBase = __instance.GetComponent<KAnimControllerBase>();
+                if (kAnimBase != null)
+                {
+                    if (__instance.name == "GasRefigerationUnitComplete")
+                    {
+                        float r = 255;
+                        float g = 190;
+                        float b = 102;
+                        kAnimBase.TintColour = new Color(r / 255f, g / 255f, b / 255f);
+                    }
+                }
+            }
+        }
+
+        // Again based off of Pholith's code.
+        [HarmonyPatch(typeof(GeneratedBuildings))]
+        [HarmonyPatch("LoadGeneratedBuildings")]
+        public class BuildingInfoPatch
+        {
+            public static LocString GRU_NAME = new LocString("Gas Refigeration Unit",
+                "STRINGS.BUILDINGS.PREFABS." + GasRefrigerationUnitConfig.ID.ToUpper() + ".NAME");
+
+            public static LocString GRU_DESC = new LocString("Cools its immediate vicinity, but heats the gas piped through it.",
+                "STRINGS.BUILDINGS.PREFABS." + GasRefrigerationUnitConfig.ID.ToUpper() + ".DESC");
+
+            public static LocString GRU_EFFECT = new LocString("Transforms " + STRINGS.UI.FormatAsLink("Dirty water", "DIRTYWATER") + " into " + STRINGS.UI.FormatAsLink("Polluted oxygen", "CONTAMINATEDOXYGEN") + " and " + STRINGS.UI.FormatAsLink("Hydrogen", "HYDROGEN") + ".",
+                "STRINGS.BUILDINGS.PREFABS." + GasRefrigerationUnitConfig.ID.ToUpper() + ".EFFECT");
+
+
+
+            static void Prefix()
+            {
+                Strings.Add(GRU_NAME.key.String, GRU_NAME.text);
+                Strings.Add(GRU_DESC.key.String, GRU_DESC.text);
+                Strings.Add(GRU_EFFECT.key.String, GRU_EFFECT.text);
+                ModUtil.AddBuildingToPlanScreen("Utilities", GasRefrigerationUnitConfig.ID);
+            }
+
+            static void Postfix()
+            {
+                object obj = Activator.CreateInstance(typeof(GasRefrigerationUnitConfig));
+                BuildingConfigManager.Instance.RegisterBuilding(obj as IBuildingConfig);
+            }
+
+        }
+        [HarmonyPatch(typeof(Db), "Initialize")]
+        public class DbPatch
+        {
+            public static void Prefix()
+            {
+                Debug.Log("Adding new buildings to technologies!");
+                Db.Get().Techs.Get("HVAC").unlockedItemIDs.Add(GasRefrigerationUnitConfig.ID);
+            }
+        }
+    }
+}
