8313f572d6f18a650386ac43bd33e84be983b586
RedstoneParadox <spinjitzu00@gmail.com>
2021-08-27 12:44:49 -0700

    Redid checks for convertability & added logic for clog checking

--- HVAC Expansion/Buildings/FluidConverter.cs
+++ HVAC Expansion/Buildings/FluidConverter.cs
@@ -29,11 +29,26 @@ namespace HVACExpansion.Buildings
                 {
                     return false;
                 }
-                else if (primaryElement.Mass <= 0.0f)
+            }
+
+            return true;
+        }
+
+        public bool IsClogged()
+        {
+            List<GameObject> items = storage.items;
+
+            for (int index = 0; index < items.Count; ++index)
+            {
+                GameObject go = items[index];
+                PrimaryElement primaryElement = go.GetComponent<PrimaryElement>();
+                Element element = primaryElement.Element;
+
+                if ((IsEvaporator() && element.IsLiquid && !element.highTempTransition.IsGas) || (!IsEvaporator() && element.IsGas && !element.lowTempTransition.IsLiquid))
                 {
                     return false;
                 }
-                else if ((IsEvaporator() && !primaryElement.Element.highTempTransition.IsGas) || (!IsEvaporator() && primaryElement.Element.highTempTransition.IsGas))
+                if (element.highTempTransitionOreMassConversion >= 0 || element.lowTempTransitionOreMassConversion >= 0)
                 {
                     return false;
                 }
@@ -89,14 +104,15 @@ namespace HVACExpansion.Buildings
             public State off;
             public State on;
             public State converting;
+            public State clogged;
 
             public override void InitializeStates(out BaseState default_state)
             {
-                base.InitializeStates(out default_state);
-                defaultState = off;
+                default_state = off;
                 root.EventTransition(GameHashes.OperationalChanged, off, smi => !smi.master.operational.IsOperational);
                 root.EventTransition(GameHashes.OperationalChanged, on, smi => smi.master.operational.IsOperational);
-                root.EventTransition(GameHashes.OnStorageChange, converting, smi => smi.master.CanConvert());
+                root.EventTransition(GameHashes.OnStorageChange, converting, smi => smi.master.CanConvert() && !smi.master.IsClogged());
+                root.EventTransition(GameHashes.OnStorageChange, clogged, smi => smi.master.IsClogged());
                 converting
                     .Enter("Ready", smi => smi.master.operational.SetActive(true))
                     .EventHandler(GameHashes.OnStorageChange, smi => smi.master.Convert())
