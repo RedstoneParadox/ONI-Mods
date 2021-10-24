b5da32d1f90515a1e8e5f1a1623a311ce4069e9f
RedstoneParadox <spinjitzu00@gmail.com>
2021-10-24 16:01:53 -0700

    Tried to fix tags not being added

--- HVAC Expansion/Patches.cs
+++ HVAC Expansion/Patches.cs
@@ -137,18 +137,23 @@ namespace HVACExpansion
         {
             public static void Postfix(ElementLoader.ElementEntry entry, Element elem)
             {
-                if (elem.IsLiquid && elem.highTempTransitionOreMassConversion == 0 && elem.highTempTransition != null && elem.highTempTransition.IsGas)
+                Debug.Log("Postfix CopyEntryToElement!");
+
+                if (elem.IsLiquid && (elem.highTempTransitionOreMassConversion == 0 || elem.highTempTransitionOreID == (SimHashes)0) && elem.highTempTransition != null && elem.highTempTransition.IsGas)
                 {
                     List<Tag> tags = new List<Tag>(new Tag[] { HVACTags.FullyEvaporatable });
                     tags.AddRange(elem.oreTags);
                     elem.oreTags = tags.ToArray();
 
+                    Debug.Log($"Added FullyEvaporatable to liquid element: ${elem.name}" );
                 }
-                else if (elem.IsGas && elem.lowTempTransitionOreMassConversion == 0 && elem.lowTempTransition != null && elem.lowTempTransition.IsLiquid)
+                else if (elem.IsGas && (elem.lowTempTransitionOreMassConversion == 0 || elem.lowTempTransitionOreID == (SimHashes)0) && elem.lowTempTransition != null && elem.lowTempTransition.IsLiquid)
                 {
                     List<Tag> tags = new List<Tag>(new Tag[] { HVACTags.FullyCondensable });
                     tags.AddRange(elem.oreTags);
                     elem.oreTags = tags.ToArray();
+
+                    Debug.Log($"Added FullyCondensable to gas element: ${elem.name}");
                 }
             }
         }
