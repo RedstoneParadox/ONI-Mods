ef43dcc553b7f468b695f6f7b160073bcf81bc45
RedstoneParadox <spinjitzu00@gmail.com>
2021-12-29 16:24:08 -0800

    Thermo Conditioner gas sprite is now properly tinted when the input gas changes

--- HVAC Expansion/Buildings/RefrigerationUnit.cs
+++ HVAC Expansion/Buildings/RefrigerationUnit.cs
@@ -3,33 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace HVACExpansion.Buildings
 {
-    class RefrigerationUnit: AirConditioner, ISimEveryTick
+    class RefrigerationUnit: AirConditioner
     {
+        private int listener = 0;
+
         public Storage GetStorage()
         {
             return storage;
         }
 
-        public void SimEveryTick(float dt)
+        protected override void OnPrefabInit()
+        {
+            base.OnPrefabInit();
+
+            listener = Subscribe(Convert.ToInt32(GameHashes.OnStorageChange), obj => UpdateTint());
+        }
+
+        protected override void OnCleanUp()
+        {
+            base.OnCleanUp();
+            Unsubscribe(listener);
+        }
+
+        public void UpdateTint()
         {
             var controller = GetComponent<KBatchedAnimController>();
             var items = storage.GetItems().ToArray();
 
-            if (items.Length <= 0) return;
-
-            var primaryElement = items[0].GetComponent<PrimaryElement>();
-            var color = primaryElement.Element.substance.uiColour;
-
-            if (isLiquidConditioner)
+            foreach (GameObject item in items)
             {
-                // TODO: Tint liquid sprites once the liquid kanim is added.
-            }
-            else
-            {
-                controller.SetSymbolTint("gas", color);
+                var primaryElement = item.GetComponent<PrimaryElement>();
+                var color = primaryElement.Element.substance.uiColour;
+
+                if (primaryElement.Mass > 0)
+                {
+                    if (isLiquidConditioner)
+                    {
+                        // TODO: Tint liquid sprites once the liquid kanim is added.
+                        break;
+                    }
+                    else
+                    {
+                        controller.SetSymbolTint("gas", color);
+                        break;
+                    }
+                }
             }
         }
     }
