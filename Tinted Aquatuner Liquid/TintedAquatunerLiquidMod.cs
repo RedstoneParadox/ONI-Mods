53a86e23dac9af4758885c00e9085c29e1eb5d43
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-30 12:59:32 -0800

    Mod main class

---
+++ Tinted Aquatuner Liquid/TintedAquatunerLiquidMod.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tinted_Aquatuner_Liquid
+{
+    class TintedAquatunerLiquidMod: UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
