238ed7cc5b908bc0ab16c9b7afaebef94777171a
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-17 14:13:51 -0700

    add UserMod2 class

---
+++ Tech Lib/TechLibMod.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using KMod;
+
+namespace TechLib
+{
+    public class TechLibMod: UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            // let the game patch everything
+            base.OnLoad(harmony);
+        }
+    }
+}
