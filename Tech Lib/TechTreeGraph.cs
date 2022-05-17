328b6a2e98353fb1c02596203cfe975535643bed
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-17 16:33:44 -0700

    Create TechTreeGraph.cs

---
+++ Tech Lib/TechTreeGraph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechLib
+{
+    class TechTreeGraph
+    {
+        List<GraphTitle> graphTitles = new List<GraphTitle>();
+
+        public void AddTitle(string titleID)
+        {
+            graphTitles.Add(new GraphTitle()
+            {
+                name = titleID
+            });
+        }
+
+        public struct GraphTitle
+        {
+            public string name;
+        }
+    }
+}
