28bb8e11417294d42551ad0b708243f20968054f
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-19 16:57:09 -0700

    foo

--- Tech Lib/TechTreeGraph.cs
+++ Tech Lib/TechTreeGraph.cs
@@ -9,10 +9,44 @@ namespace TechLib
     class TechTreeGraph
     {
         private List<GraphTitle> graphTitles = new List<GraphTitle>();
+        private Dictionary<string, GraphTech> techsReference = new Dictionary<string, GraphTech>();
 
         public void AddTitle(string titleID)
         {
             graphTitles.Add(new GraphTitle(titleID));
+        }
+
+        public void AddTech(string techID)
+        {
+            if (techsReference.ContainsKey(techID))
+            {
+                Debug.LogError($"Tried to add tech '{techID}' to graph when it already exists.");
+            }
+        }
+
+        private int CalculateColumn(List<GraphTech> dependencies)
+        {
+            int column = -1;
+
+            foreach (GraphTitle title in graphTitles)
+            {
+                foreach (var entry in title.Columns)
+                {
+                    int index = entry.Key;
+                    List<GraphTech> columnTechs = entry.Value;
+
+                    foreach (GraphTech dependency in dependencies)
+                    {
+                        if (columnTechs.Contains(dependency))
+                        {
+                            column = index;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return column + 1;
         }
 
         public class GraphTitle
@@ -29,11 +63,17 @@ namespace TechLib
         public class GraphTech
         {
             public readonly string Name;
+            public readonly int Column;
             public List<GraphTech> dependencies = new List<GraphTech>();
 
             public GraphTech(string name)
             {
                 Name = name;
+            }
+
+            public override int GetHashCode()
+            {
+                return Name.GetHashCode();
             }
         }
     }
