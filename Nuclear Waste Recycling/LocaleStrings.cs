b527ebc2601f258f6b9fbf375a551c0e832b3c75
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-20 12:26:41 -0700

    Localize everything

---
+++ Nuclear Waste Recycling/LocaleStrings.cs
@@ -0,0 +1,36 @@
+﻿using NuclearWasteRecycling.Content;
+using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuclearWasteRecycling
+{
+    class LocaleStrings
+    {
+        public class ELEMENTS
+        {
+            public class MOXFUEL
+            {
+                public static LocString NAME = new LocString(UI.FormatAsLink("Radium MOX Fuel", Elements.MOXFuel.ToString()), "STRINGS.ELEMENTS.MOXFUEL.NAME");
+                public static LocString DESC = new LocString("A nuclear fuel made from reactor waste products.", "STRINGS.ELEMENTS.MOXFUEL.DESC");
+            }
+        }
+
+        public class BUILDINGS
+        {
+            public class PREFABS
+            {
+                public class NUCLEAR_WASTE_RECYCLER
+                {
+                    public static LocString NAME = new LocString(UI.FormatAsLink("Nuclear Waste Recycler", Elements.MOXFuel.ToString()), "STRINGS.BUILDINGS.PREFABS.NUCLEAR_WASTE_RECYCLER.NAME");
+                    public static LocString DESC = new LocString("Nuclear Waste Recyclers process the byproducts of a nuclear reaction.", "STRINGS.BUILDINGS.PREFABS.NUCLEAR_WASTE_RECYCLER.DESC");
+                    public static LocString EFFECT = new LocString($"Extracts various elements from {UI.FormatAsLink("Nuclear Waste", SimHashes.NuclearWaste.ToString())}.\n\nRadbolts can be used to change the quantities of those elements.", 
+                        "STRINGS.BUILDINGS.PREFABS.NUCLEAR_WASTE_RECYCLER.EFFECT");
+                }
+            }
+        }
+    }
+}
---
+++ Nuclear Waste Recycling/Patches/LocalizationPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuclearWasteRecycling.Patches
+{
+    public class LocalizationPatch
+    {
+        [HarmonyPatch(typeof(Localization), "Initialize")]
+        public class Localization_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                Util.AddString(LocaleStrings.ELEMENTS.MOXFUEL.NAME);
+                Util.AddString(LocaleStrings.ELEMENTS.MOXFUEL.DESC);
+
+                Util.AddString(LocaleStrings.BUILDINGS.PREFABS.NUCLEAR_WASTE_RECYCLER.NAME);
+                Util.AddString(LocaleStrings.BUILDINGS.PREFABS.NUCLEAR_WASTE_RECYCLER.DESC);
+                Util.AddString(LocaleStrings.BUILDINGS.PREFABS.NUCLEAR_WASTE_RECYCLER.EFFECT);
+            }
+        }
+    }
+}
