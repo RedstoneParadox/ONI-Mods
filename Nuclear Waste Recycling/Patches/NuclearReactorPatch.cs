ebd600d08c22476d9d8111dc0a6b7af03de8fb08
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-20 22:49:48 -0700

    Patch nuclear reactor to switch to mox fuel

--- Nuclear Waste Recycling/Patches/NuclearReactorPatch.cs
+++ Nuclear Waste Recycling/Patches/NuclearReactorPatch.cs
@@ -12,15 +12,36 @@ namespace NuclearWasteRecycling.Patches
     class NuclearReactorPatch
     {
         [HarmonyPatch(typeof(NuclearReactorConfig), "ConfigureBuildingTemplate")]
-        public class UraniumCentrifugeConfig_ConfigureBuildingTemplate_Patch
+        public class NuclearReactorConfig_ConfigureBuildingTemplate_Patch
         {
             public static void Postfix(ref GameObject go)
             {
                 ManualDeliveryKG manualDeliveryKg = go.GetComponent<ManualDeliveryKG>();
                 manualDeliveryKg.RequestedItemTag = Tags.ReactorFuel;
+            }
+        }
 
-                Reactor reactor = go.AddOrGet<Reactor>();
-                Traverse.Create(reactor).Field("fuelTag").SetValue(Tags.ReactorFuel);
+        [HarmonyPatch(typeof(Reactor), "GetStoredFuel")]
+        public class Reactor_GetStoredFuel_Patch
+        {
+            public static void Prefix(ref Reactor __instance)
+            {
+                Storage supplyStorage = Traverse.Create(__instance).Field("supplyStorage").GetValue<Storage>();
+                Storage reactionStorage = Traverse.Create(__instance).Field("reactionStorage").GetValue<Storage>();
+                var fuelTagField = Traverse.Create(__instance).Field<Tag>("fuelTag");
+                
+                if (reactionStorage.GetMassAvailable(fuelTagField.Value) < 0.25f)
+                {
+                    foreach (GameObject go in supplyStorage.items)
+                    {
+                        PrimaryElement primary = go.GetComponent<PrimaryElement>();
+
+                        if (primary != null && primary.Element.oreTags.Contains(Tags.ReactorFuel))
+                        {
+                            fuelTagField.Value = primary.Element.tag;
+                        }
+                    }
+                }
             }
         }
     }
