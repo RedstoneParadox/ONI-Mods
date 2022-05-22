6d9d9e67343ac28701b964e995eb59a2faed3453
RedstoneParadox <spinjitzu00@gmail.com>
2022-05-22 06:39:15 -0700

    Add StorageLiquidTint to liquid pump

--- Liquid Tint/Patches.cs
+++ Liquid Tint/Patches.cs
@@ -6,12 +6,21 @@ namespace LiquidTint
     public class Patches
     {
         [HarmonyPatch(typeof(LiquidConditionerConfig),"DoPostConfigureComplete")]
-        public class LiquidConditionerConfig_DoPostConfigureCompletePatch
+        public class LiquidConditionerConfig_DoPostConfigureComplete_Patch
         {
             public static void Prefix(GameObject go)
             {
                 StorageLiquidTint tint = go.AddOrGet<StorageLiquidTint>();
                 tint.symbols = new string[] { "liquid" };
+            }
+        }
+
+        public class LiquidPumpConfig_DoPostConfigureComplete_Patch
+        {
+            public static void Prefix(GameObject go)
+            {
+                StorageLiquidTint tint = go.AddOrGet<StorageLiquidTint>();
+                tint.symbols = new string[] { "water" };
             }
         }
     }
