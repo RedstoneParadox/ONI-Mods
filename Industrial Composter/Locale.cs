45e1a65516ee52e1d82e1f11ff92a34f5eb02b61
RedstoneParadox <spinjitzu00@gmail.com>
2025-07-21 16:16:00 -0700

    Fixed local issue

--- Industrial Composter/Locale.cs
+++ Industrial Composter/Locale.cs
@@ -16,8 +16,8 @@ namespace Industrial_Composter
                 );
             public static LocString DESC = new LocString("Mechanized composter to eliminate manual labor.",
                 "STRINGS.BUILDINGS.PREFABS." + IndustrialComposterConfig.ID.ToUpper() + ".DESC");
-            public static LocString EFFECT = new LocString("Converts " + STRINGS.UI.FormatAsLink("Compostables", "COMPOSTABLES") + " into " + STRINGS.UI.FormatAsLink("dirt", "DIRT") +
-                "In a much higher volume than its manual counterpart.",
+            public static LocString EFFECT = new LocString("Converts " + STRINGS.UI.FormatAsLink("Compostables", "COMPOSTABLE") + " into " + STRINGS.UI.FormatAsLink("dirt", "DIRT") +
+                " in a much higher volume than its manual counterpart.",
                 "STRINGS.BUILDINGS.PREFABS." + IndustrialComposterConfig.ID.ToUpper() + ".EFFECT"
                 );
         }
