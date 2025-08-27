6ad1428e90e76e72bb8e34f9dab8ee3577913a63
RedstoneParadox <spinjitzu00@gmail.com>
2025-08-27 15:03:30 -0700

    Set up eni project

---
+++ EnergisticsNotIncluded/EnergisticsNotIncludedMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using KMod;
+
+namespace EnergisticsNotIncluded
+{
+    public class EnergisticsNotIncludedMod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
