7ec956f693452d5c9a9d4c9cdfea8ae0b3b32668
RedstoneParadox <spinjitzu00@gmail.com>
2025-08-27 15:25:07 -0700

    Dummy ME network

--- EnergisticsNotIncluded/EnergisticsNotIncludedMod.cs
+++ EnergisticsNotIncluded/EnergisticsNotIncludedMod.cs
@@ -1,10 +1,12 @@
 ﻿using HarmonyLib;
+using EnergisticsNotIncluded.Buildings;
 using KMod;
 
 namespace EnergisticsNotIncluded
 {
     public class EnergisticsNotIncludedMod : UserMod2
     {
+        internal static UtilityNetworkManager<MEUtilityNetwork, MECable> meUtilityNetwork;
         public override void OnLoad(Harmony harmony)
         {
             base.OnLoad(harmony);
---
+++ EnergisticsNotIncluded/Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergisticsNotIncluded
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(Game), "OnPrefabInit")]
+        public class Game_OnPrefabInit
+        {
+            public static void Postfix()
+            {
+                EnergisticsNotIncludedMod.meUtilityNetwork = new UtilityNetworkManager<Buildings.MEUtilityNetwork, Buildings.MECable>(Grid.WidthInCells, Grid.HeightInCells, 27);
+                Debug.Log("Here!");
+            }
+        }
+    }
+}
